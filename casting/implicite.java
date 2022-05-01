5d305a3751e178e79f653d882b255b8e596fc383
Sunilmunde1996 <102177256+Sunilmunde1996@users.noreply.github.com>
2022-05-01 17:08:00 +0530

    CASTING

---
+++ casting/implicite.java
@@ -0,0 +1,16 @@
+package casting;
+
+public class implicite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	byte a=30;   // lower data type to higher data type means implicite casting
+	double b=a;
+	System.out.println(b);// data loss not  occurs 
+		
+		
+		
+	}
+
+}
