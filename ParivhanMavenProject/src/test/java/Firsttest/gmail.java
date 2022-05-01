74d71cabe77537ebbad7a83151df37c217133295
Sunilmunde1996 <102177256+Sunilmunde1996@users.noreply.github.com>
2022-05-01 17:23:25 +0530

    parivhan projecrt

---
+++ ParivhanMavenProject/src/test/java/Firsttest/gmail.java
@@ -0,0 +1,12 @@
+package Firsttest;
+
+
+public class gmail {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\selenium\\selenium\\chromedriver_win32\\Chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+	}
+
+}
---
+++ ParivhanMavenProject/src/test/resources/bb.java
@@ -0,0 +1,13 @@
+
+public class bb {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver","D:\\selenium\\selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+driver.get("https://mvnrepository.com/artifact/javax.xml.bind/jaxb-api/2.4.0-b180830.0359");
+	
+
+	}
+
+}
